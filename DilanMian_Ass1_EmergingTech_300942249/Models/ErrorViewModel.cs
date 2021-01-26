fc368eeb3d710e6586f01170f663ec3e7a7d2eaf
Dilan Mian <miandilan@gmail.com>
2021-01-25 20:11:58 -0500

    Added Project files

---
+++ DilanMian_Ass1_EmergingTech_300942249/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DilanMian_Ass1_EmergingTech_300942249.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ DilanMian_Ass1_EmergingTech_300942249/Models/MyModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DilanMian_Ass1_EmergingTech_300942249.Models
+{
+    public class MyModel
+    {
+    }
+}
